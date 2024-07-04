d1479521db978ddedd8816a5a9717b19c914bcbd
laura.alves <laura.alves@gocase.com>
2024-07-03 23:52:07 -0300

    refactor: clean code

--- repositories/PokemonRepository.ts
+++ repositories/PokemonRepository.ts
@@ -5,13 +5,11 @@ const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 export default {
   async fetchPokemonList(): Promise<Pokemon[]> {
-    console.log('fetchPokemonList');
     const response = await axios.get(`${BASE_URL}`);
     return response.data.results.map((pokemon: any, index: number) => new Pokemon(index + 1, pokemon.name, 0, 0, []));
   },
 
   async fetchPokemonById(id: number): Promise<Pokemon> {
-    console.log('fetchPokemonById');
     const response = await axios.get(`${BASE_URL}/${id}`);
     const { name, height, weight, types } = response.data;
     return new Pokemon(id, name, height, weight, types);

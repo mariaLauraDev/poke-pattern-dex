7a9d54a37828dfa97323473de4bd67fa74e66d0c
laura.alves <laura.alves@gocase.com>
2024-07-03 22:08:31 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  </h1>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path'
+
+export default defineNuxtConfig({
+  alias: {
+    '~': resolve(__dirname, '/'),
+  },
+  css: ['~/assets/css/main.scss'],
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  buildModules: [
+    '@pinia/nuxt',
+  ],
+})
